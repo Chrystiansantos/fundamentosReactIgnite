86cc167158e91a50e7b013d2e73cf40b39aabb88
chrystiansantos <chrystian_santos_silva@hotmail.com>
2021-03-10 22:58:01 -0300

    configurando eslint, usando TS, fazendo requisição

--- src/components/RepositoryItem.jsx
+++
@@ -1,9 +0,0 @@
-export function RepositoryItem(props) {
-  return (
-    <li>
-      <strong>{props.repository?.name ?? 'batata'}</strong>
-      <p>{props.repository?.description ?? 'macarrao'}</p>
-      <a href={props.repository?.link}>{props.repository?.link ?? 'abacaxi'}</a>
-    </li>
-  );
-}
